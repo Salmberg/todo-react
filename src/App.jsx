5987c91d25d5667780c79baf2ea4fc2720807810
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-09-18 11:29:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const Counter = () => {
+  const [count, setCount ] = useState(0);
+
+  const increment = () => {
+    setCount(count+1);
+  }
+  const decrement = () => {
+    setCount(count-1);
+  }
+
+
+  return (
+    <div>
+    
+    <h1>Räknare</h1>
+    <p>Antal: {count}</p>
+    <button onClick={increment}>Öka</button>
+    <button onClick={decrement}>Minska</button>
+
+    </div>
+
+  )
+}
+
+function App() {
+
+
+
+  return (
+    <div className='App'>
+      <Counter />
+      </div>
+  )
+}
+
+export default App
